b0b7b2e21c3cbc30a5cd489ef6e3d1cacdfc9f1c
Babatunde Alaraje <babatunde.alaraje@karsun-llc.com>
2025-02-05 23:09:45 -0600

    added allowed host

--- frobarnapp/settings.py
+++ frobarnapp/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = ['kacytunde.com', 'www.kacytunde.com', '127.0.0.1', 'localhost']
