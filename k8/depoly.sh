8493c4fed3fa56863cd8f610eb32476d67d4fd02
Babatunde Alaraje <babatunde.alaraje@karsun-llc.com>
2025-02-23 00:32:39 -0600

    update

--- k8/depoly.sh
+++ k8/depoly.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-kubectl delete -f dev
+# kubectl delete -f dev
 kubectl apply -f dev
 
-kubectl delete -f prod
+# kubectl delete -f prod
 kubectl apply -f prod
 
-kubectl delete -f test
+# kubectl delete -f test
 kubectl apply -f test
 
-kubectl get ingress
+# kubectl get ingress
 
 sleep 10
 
